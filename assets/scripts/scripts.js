03838c418e0ffe4eed2bf81c46f2e980f7cbfe2e
Matias Di Iorio <matiasmd8610@gmail.com>
2025-06-02 18:49:11 -0300

    Updates contacto/thank you page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,7 +2,7 @@
 *****************************************************/
 window.addEventListener('scroll', function () {
   var header = document.querySelector('header');
-  header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 50);
 });
 
 /*Menu mobile
